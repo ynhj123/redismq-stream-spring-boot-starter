f5600ffcf2ad09a689cbceeb70c15eb3173febf1
yangniuhaojiang <542182536@qq.com>
2021-08-04 00:33:57 +0800

    [feature] commit init

---
+++ src/main/java/com/ynhj123/redismqstreamspringbootstart/RedismqStreamSpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package com.ynhj123.redismqstreamspringbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismqStreamSpringBootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedismqStreamSpringBootStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ynhj123/redismqstreamspringbootstart/RedismqStreamSpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynhj123.redismqstreamspringbootstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedismqStreamSpringBootStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
